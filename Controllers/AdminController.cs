d0f07a602c8992d4aa142449e08cd2814ffbd80d
Piedone <piedone@pyrocenter.hu>
2014-10-11 23:44:17 +0200

    Implementing basic functionality

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Security;
+using Orchard.Mvc.Extensions;
+using Orchard.UI.Admin;
+
+namespace Lombiq.LoginAsAnybody.Controllers
+{
+    [Admin]
+    public class AdminController : Controller
+    {
+        private readonly IAuthorizer _authorizer;
+        private readonly IMembershipService _membershipService;
+        private readonly IAuthenticationService _authenticationService;
+
+
+        public AdminController(
+            IAuthorizer authorizer,
+            IMembershipService membershipService,
+            IAuthenticationService authenticationService)
+        {
+            _authorizer = authorizer;
+            _membershipService = membershipService;
+            _authenticationService = authenticationService;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string userName, string returnUrl = null)
+        {
+            if (!_authorizer.Authorize(StandardPermissions.SiteOwner)) return new HttpUnauthorizedResult();
+
+            var user = _membershipService.GetUser(userName);
+
+            if (user == null) return HttpNotFound();
+
+            _authenticationService.SignIn(user, false);
+
+            if (string.IsNullOrEmpty(returnUrl)) returnUrl = "~/";
+
+            return this.RedirectLocal(returnUrl);
+        }
+    }
+}
