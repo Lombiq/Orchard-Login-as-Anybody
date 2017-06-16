280ecedfde3e0f6b2c71e55877c97ae87ccfd87d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-06-16 04:03:17 +0200

    Fixing that if a user explicitly logged out we couldn't log in with it
    --HG--
    branch : dev

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Orchard.Security;
 using Orchard.Mvc.Extensions;
 using Orchard.UI.Admin;
+using Orchard.Users.Events;
 
 namespace Lombiq.LoginAsAnybody.Controllers
 {
@@ -15,16 +16,20 @@ namespace Lombiq.LoginAsAnybody.Controllers
         private readonly IAuthorizer _authorizer;
         private readonly IMembershipService _membershipService;
         private readonly IAuthenticationService _authenticationService;
+        // Orchard.Users is not a dependency in the module manifest because it needn't be turned on.
+        private readonly IUserEventHandler _userEventHandler;
 
 
         public AdminController(
             IAuthorizer authorizer,
             IMembershipService membershipService,
-            IAuthenticationService authenticationService)
+            IAuthenticationService authenticationService,
+            IUserEventHandler userEventHandler)
         {
             _authorizer = authorizer;
             _membershipService = membershipService;
             _authenticationService = authenticationService;
+            _userEventHandler = userEventHandler;
         }
 
 
@@ -43,6 +48,8 @@ namespace Lombiq.LoginAsAnybody.Controllers
             if (user == null) return HttpNotFound();
 
             _authenticationService.SignIn(user, false);
+
+            _userEventHandler.LoggedIn(user);
 
             if (string.IsNullOrEmpty(returnUrl)) returnUrl = "~/";
 
