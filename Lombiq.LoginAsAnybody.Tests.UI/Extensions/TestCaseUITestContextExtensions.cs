ea5fac782cf3e5350fe9e727ee8117882c943e48
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-11-28 02:09:15 +0100

    Fixing tests

--- Lombiq.LoginAsAnybody.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.LoginAsAnybody.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,40 +1,36 @@
 using Lombiq.Tests.UI.Extensions;
-using Lombiq.Tests.UI.Models;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
 using System.Threading.Tasks;
+using static Lombiq.Tests.UI.Constants.TestUser;
 
 namespace Lombiq.LoginAsAnybody.Tests.UI.Extensions;
 
 public static class TestCaseUITestContextExtensions
 {
-    public static async Task TestLoginAsAnybodyAsync(this UITestContext context)
+    public static async Task SwitchingUserShouldWorkCorrectlyAsync(this UITestContext context)
     {
-        await context.SignInDirectlyAndGoToDashboardAsync();
+        await context.CreateUserAsync();
 
-        var userParameters = UserRegistrationParameters.CreateDefault();
-        await context.CreateUserAsync(userParameters.UserName, userParameters.Password, userParameters.Email);
-
+        await context.SignInDirectlyAsync();
         await context.GoToUsersAsync();
         await context.ClickReliablyOnAsync(By.XPath("//a[contains(.,'Log in as user')]"));
 
-        (await context.GetCurrentUserNameAsync()).ShouldBe(userParameters.UserName);
+        (await context.GetCurrentUserNameAsync()).ShouldBe(UserName);
     }
 
-    public static async Task TestLoginAsAnybodyAuthorizationAsync(this UITestContext context)
+    public static async Task PermissionCheckShouldWorkCorrectlyAsync(this UITestContext context)
     {
-        var userParameters = UserRegistrationParameters.CreateDefault();
-        await context.CreateUserAsync(userParameters.UserName, userParameters.Password, userParameters.Email);
+        await context.CreateUserAsync();
 
         // The role needs this permission to visit the users page.
         await context.AddPermissionToRoleAsync("ManageUsers", "Moderator");
-        await context.AddUserToRoleAsync(userParameters.UserName, "Moderator");
+        await context.AddUserToRoleAsync(UserName, "Moderator");
 
-        await context.SignInDirectlyAndGoToDashboardAsync(userParameters.UserName);
+        await context.SignInDirectlyAsync(UserName);
         await context.GoToUsersAsync();
 
-        await context.ClickReliablyOnAsync(By.XPath("//a[contains(.,'Log in as user')]"));
-        context.Exists(By.XPath("//h1[contains(text(),'You are not authorized to view this content.')]"));
+        context.Missing(By.XPath("//a[contains(.,'Log in as user')]"));
     }
 }
